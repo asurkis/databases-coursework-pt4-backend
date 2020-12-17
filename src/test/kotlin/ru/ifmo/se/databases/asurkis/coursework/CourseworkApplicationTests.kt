211fa27d7ecda40435d2ad959b98b736d432d3b9
Anton Surkis <antonsurkis@gmail.com>
2020-12-17 13:39:19 +0300

    Initial commit

---
+++ src/test/kotlin/ru/ifmo/se/databases/asurkis/coursework/CourseworkApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ifmo.se.databases.asurkis.coursework
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
